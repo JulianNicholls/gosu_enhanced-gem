c7028e22394dd7531dd50087dd3374cb040047b1
Julian Nicholls <juliannicholls29@gmail.com>
2014-04-19 10:13:46 +0100

    A Point can now be offseted and moved by a Size.

--- lib/gosu_enhanced/point.rb
+++ lib/gosu_enhanced/point.rb
@@ -4,17 +4,22 @@ module GosuEnhanced
     def offset( by_x, by_y = nil )
       if by_x.respond_to? :x
         Point.new( x + by_x.x, y + by_x.y )
+      elsif by_x.respond_to? :width
+        Point.new( x + by_x.width, y + by_x.height )
       else
         Point.new( x + by_x, y + by_y )
       end
     end
 
     # Negative co-ordinates are allowed.
-    
+
     def move_by!( by_x, by_y = nil )
       if by_x.respond_to? :x
         self.x += by_x.x
         self.y += by_x.y
+      elsif by_x.respond_to? :width
+        self.x += by_x.width
+        self.y += by_x.height
       else
         self.x += by_x
         self.y += by_y
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -10,25 +10,32 @@ describe GosuEnhanced::Point do
 
     it 'should work with negative values' do
       point = GosuEnhanced::Point.new( -10, 20 )
-      expect( point.x ).to eq -10
+      expect( point.x ).to eq( -10 )
       expect( point.y ).to eq 20
 
       point = GosuEnhanced::Point.new( 10, -20 )
       expect( point.x ).to eq 10
-      expect( point.y ).to eq -20
+      expect( point.y ).to eq( -20 )
 
       point = GosuEnhanced::Point.new( -10, -20 )
-      expect( point.x ).to eq -10
-      expect( point.y ).to eq -20
+      expect( point.x ).to eq( -10 )
+      expect( point.y ).to eq( -20 )
     end
   end
-  
+
   describe '#offset' do
     it 'should work with another Point' do
       point  = GosuEnhanced::Point.new( 10, 20 )
       opoint = point.offset( GosuEnhanced::Point.new( 30, 40 ) )
       expect( opoint.x ).to eq 40
       expect( opoint.y ).to eq 60
+    end
+
+    it 'should work with a Size' do
+      point  = GosuEnhanced::Point.new( 10, 20 )
+      opoint = point.offset( GosuEnhanced::Size.new( 50, 60 ) )
+      expect( opoint.x ).to eq 60
+      expect( opoint.y ).to eq 80
     end
 
     it 'should work with two positive values' do
@@ -48,11 +55,11 @@ describe GosuEnhanced::Point do
     it 'should work with values that result in negative co-ordinates' do
       point  = GosuEnhanced::Point.new( 60, 80 )
       opoint = point.offset( -80, -150 )
-      expect( opoint.x ).to eq -20
-      expect( opoint.y ).to eq -70
+      expect( opoint.x ).to eq( -20 )
+      expect( opoint.y ).to eq( -70 )
     end
   end
-  
+
   describe '#move_by!' do
     it 'should work with another Point' do
       point  = GosuEnhanced::Point.new( 10, 20 )
@@ -60,7 +67,14 @@ describe GosuEnhanced::Point do
       expect( point.x ).to eq 40
       expect( point.y ).to eq 60
     end
-    
+
+    it 'should work with a Size' do
+      point  = GosuEnhanced::Point.new( 10, 20 )
+      point.move_by!( GosuEnhanced::Size.new( 40, 50 ) )
+      expect( point.x ).to eq 50
+      expect( point.y ).to eq 70
+    end
+
     it 'should work with two positive values' do
       point  = GosuEnhanced::Point.new( 10, 20 )
       point.move_by!( 40, 50 )
@@ -78,11 +92,11 @@ describe GosuEnhanced::Point do
     it 'should work with values that result in negative co-ordinates' do
       point  = GosuEnhanced::Point.new( 60, 80 )
       point.move_by!( -80, -150 )
-      expect( point.x ).to eq -20
-      expect( point.y ).to eq -70
+      expect( point.x ).to eq( -20 )
+      expect( point.y ).to eq( -70 )
     end
   end
-  
+
   describe '#move_to!' do
     it 'should work with another Point' do
       point  = GosuEnhanced::Point.new( 10, 20 )
@@ -90,7 +104,7 @@ describe GosuEnhanced::Point do
       expect( point.x ).to eq 30
       expect( point.y ).to eq 40
     end
-    
+
     it 'should work with two positive values' do
       point  = GosuEnhanced::Point.new( 10, 20 )
       point.move_to!( 40, 50 )
@@ -101,26 +115,30 @@ describe GosuEnhanced::Point do
     it 'should work with two negative values' do
       point  = GosuEnhanced::Point.new( 60, 80 )
       point.move_to!( -40, -50 )
-      expect( point.x ).to eq -40
-      expect( point.y ).to eq -50
+      expect( point.x ).to eq( -40 )
+      expect( point.y ).to eq( -50 )
     end
   end
-  
+
   # This might seem laboured, but I have run foul of dup before
-  
+
   describe '#dup' do
     it 'should make a copy of the Point' do
       point  = GosuEnhanced::Point.new( 10, 20 )
       npoint = point.dup
+
       expect( point.x ).to eq 10
       expect( point.y ).to eq 20
+
       expect( npoint.x ).to eq 10
       expect( npoint.y ).to eq 20
-      point.move_to!( 40, 50 )
-      expect( point.x ).to eq 40
-      expect( point.y ).to eq 50
-      expect( npoint.x ).to eq 10
-      expect( npoint.y ).to eq 20
+
+      npoint.move_to!( 40, 50 )
+      expect( npoint.x ).to eq 40
+      expect( npoint.y ).to eq 50
+
+      expect( point.x ).to eq 10
+      expect( point.y ).to eq 20
     end
   end
 end

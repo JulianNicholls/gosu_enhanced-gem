ab5f4e1ae17f053ed01062c4dcdeb5ad88939cb8
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-17 21:44:41 +0100

    Added documentation to Gosu::Font / Window

--- lib/gosu_enhanced/enhanced.rb
+++ lib/gosu_enhanced/enhanced.rb
@@ -1,7 +1,16 @@
+# Gosu is the module that the Gosu library uses. It is re-opened here to allow
+# access to the Window and Font classes.
 module Gosu
   # Add a draw_rectangle() to Window which simplifies drawing a simple rectangle
   # in one colour
   class Window
+    # Simplify drawing a rectangle in a single colour.
+    #
+    # @param point [Point] Top left corner
+    # @param size [Size] Width and Height
+    # @param z_index [Fixnum] Z-order
+    # @param colour [Gosu::Color] Colour of rectangle
+
     def draw_rectangle( point, size, z_index, colour )
       left, top     = point.x, point.y
       width, height = size.width, size.height
@@ -17,13 +26,27 @@ module Gosu
   end
 
   # Add a measure to return both width and height for a text and a way
-  # to centre a text in a rectangle
+  # to centre a text in a rectangle.
   class Font
     include GosuEnhanced
+
+    # Return the width and height of a given string
+    #
+    # @param text [String] String to measure
+    #
+    # @return [Size] The height and width of the string.
 
     def measure( text )
       Size.new( text_width( text, 1 ), height )
     end
+
+    # Return the co-ordnates needed to place a given string in the centre of an
+    # area, both vertically and horizontally
+    #
+    # @param text [String] String to centre
+    # @param rect [Size] Rectangular area size
+    #
+    # @return [Point] The point to write the string, expressed as an offset from the top-left corner of the rectangle.
 
     def centred_in( text, rect )
       size = measure( text )
